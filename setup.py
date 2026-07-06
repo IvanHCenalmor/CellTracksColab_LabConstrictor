69f8eaf6c1f44ade9b2639e137d4162a53a9fb75
Iván Hidalgo <47150974+IvanHCenalmor@users.noreply.github.com>
2026-07-06 18:23:22 +0300

    Rename package from 'celltrackscolab' to 'celltracks'

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="celltrackscolab",
+    name="celltracks",
     version="0.0.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
