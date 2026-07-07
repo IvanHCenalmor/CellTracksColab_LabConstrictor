d54c806146afa6926daf7221b7b9fbabe4527037
Iván Hidalgo <47150974+IvanHCenalmor@users.noreply.github.com>
2026-07-07 09:12:20 +0300

    Revert "Rename package from 'celltrackscolab' to 'celltracks'"
    This reverts commit 69f8eaf6c1f44ade9b2639e137d4162a53a9fb75.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="celltracks",
+    name="celltrackscolab",
     version="0.0.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
